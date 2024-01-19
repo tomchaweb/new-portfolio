789bb3fe8455c69213cede0219d2e9e6002ea9db
Tom <151747854+tomchaweb@users.noreply.github.com>
2024-01-19 20:15:57 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function sendMail(){
+const getIntouchButton = document.getElementById("get-in-touch-btn");
+
+function SendMail(){
     const link = "mailto:tom.chapman97@btinternet.com";
     window.location.href = link;
 }
+
+getIntouchButton.addEventListener("click", SendMail);
