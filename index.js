d2b602349bf78cf067284529c35d153bfecd778d
Tom <151747854+tomchaweb@users.noreply.github.com>
2024-01-29 20:07:32 +0000

    Add files via upload
    Changed the styling on the CSS to give the site a cleaner look.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const getIntouchButton = document.getElementById("get-in-touch-btn");
+const footerText = document.getElementById("footer-text");
 
 function SendMail(){
     const link = "mailto:tom.chapman97@btinternet.com";
     window.location.href = link;
 }
-
 getIntouchButton.addEventListener("click", SendMail);
+
+const d = new Date();
+let year = d.getFullYear();
+footerText.textContent = `© Tom Chapman ${year}. All rights reserved.`
+
+
+
