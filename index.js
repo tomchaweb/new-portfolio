927f79f61e7622c6db03c0d5330af2bf0b9e942c
Tom <151747854+tomchaweb@users.noreply.github.com>
2024-02-11 10:51:38 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const getIntouchButton = document.getElementById("get-in-touch-btn");
-const footerText = document.getElementById("footer-text");
+const getIntouchButton = document.querySelector("#get-in-touch-btn");
+const footerText = document.querySelector("#footer-text");
 
-function SendMail(){
-    const link = "mailto:tom.chapman97@btinternet.com";
-    window.location.href = link;
+function SendMail() {
+  const link = "mailto:tom.chapman97@btinternet.com";
+  window.location.href = link;
 }
 getIntouchButton.addEventListener("click", SendMail);
 
 const d = new Date();
 let year = d.getFullYear();
-footerText.textContent = `© Tom Chapman ${year}. All rights reserved.`
-
-
-
+footerText.textContent = `© Tom Chapman ${year}. All rights reserved.`;
